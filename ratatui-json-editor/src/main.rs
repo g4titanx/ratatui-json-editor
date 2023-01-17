65a8c0b8cb966796d4289d963103d73a22a40cec
g4titanx <g4titan1@gmail.com>
2023-01-17 10:12:00 +0100

    run_app logic

--- ratatui-json-editor/src/main.rs
+++ ratatui-json-editor/src/main.rs
@@ -6,15 +6,18 @@ use crossterm::event::DisableMouseCapture;
 use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
     enable_raw_mode()?;
     let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
     execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stderr);
     let mut terminal = Terminal::new(backend)?;
 
+    // create app and run it
     let mut app = App::new();
     let res = run_app(&mut terminal, &mut app);
 
+    // restore terminal
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
@@ -23,6 +26,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
     terminal.show_cursor()?;
 
-    
+    if let Ok(do_print) = res {
+        if do_print {
+            app.print_json()?;
+        }
+    } else if let Err(err) = res {
+        println!("{err:?}");
+    }
 
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+    
 }
