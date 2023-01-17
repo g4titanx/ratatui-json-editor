9689a14acac3ad8120d5a48a573026ea663b423f
g4titanx <g4titan1@gmail.com>
2023-01-17 09:00:00 +0100

    more boilerplate to fn main

--- ratatui-json-editor/src/main.rs
+++ ratatui-json-editor/src/main.rs
@@ -2,10 +2,27 @@ use crossterm::event::EnableMouseCapture;
 use crossterm::execute;
 use crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
 use std::io;
+use crossterm::event::DisableMouseCapture;
+use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
     enable_raw_mode()?;
     let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
     execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
-    // --snip--
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    
+
+}
