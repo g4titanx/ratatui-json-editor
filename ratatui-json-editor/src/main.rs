8ed5f0ece55dcd31d8dc10040ab8cedd679e41c6
g4titanx <g4titan1@gmail.com>
2023-01-30 10:01:00 +0100

    handle events

--- ratatui-json-editor/src/main.rs
+++ ratatui-json-editor/src/main.rs
@@ -67,4 +67,19 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
                     }
                     _ => {}
                 },
+                CurrentScreen::Editing if key.kind == KeyEventKind::Press => {
+                    match key.code {
+                        KeyCode::Enter => {
+                            if let Some(editing) = &app.currently_editing {
+                                match editing {
+                                    CurrentlyEditing::Key => {
+                                        app.currently_editing = Some(CurrentlyEditing::Value);
+                                    }
+                                    CurrentlyEditing::Value => {
+                                        app.save_key_value();
+                                        app.current_screen = CurrentScreen::Main;
+                                    }
+                                }
+                            }
+                        }
 }
