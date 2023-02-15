415c48dae49e43f03d0c284b4441d6793f8bc520
g4titanx <g4titan1@gmail.com>
2023-02-15 10:01:00 +0100

    create ui

--- ratatui-json-editor/src/main.rs
+++ ratatui-json-editor/src/main.rs
@@ -119,7 +119,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
                 _ => {}
             }
         }
-        
+
         let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
@@ -128,4 +128,30 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
             Constraint::Length(3),
         ])
         .split(f.size());
+}
+
+pub fn ui(f: &mut Frame, app: &App) {
+}
+
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
 }
