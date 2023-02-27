4865d96e59675dc6ec18482095287f0b82320dda
g4titanx <g4titan1@gmail.com>
2023-02-27 10:21:00 +0100

    create ui

---
+++ ratatui-json-editor/src/ui.rs
@@ -0,0 +1,9 @@
+use ratatui::{
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style},
+    text::{Line, Span, Text},
+    widgets::{Block, Borders, Clear, List, ListItem, Paragraph, Wrap},
+    Frame,
+};
+
+use crate::app::{App, CurrentScreen, CurrentlyEditing};
